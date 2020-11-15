791f7105d054e9ffe461e310aac968aa05dcfbeb
antizyk <sashamatvienko0@gmail.com>
2020-11-15 20:01:18 +0100

    new object

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 "use strict";
 
 let numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
+let personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: {},
+	privat: false
+}
