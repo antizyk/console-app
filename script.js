87287de282c65df10f381b13ea2974567018c0bf
antizyk <sashamatvienko0@gmail.com>
2020-11-16 11:47:48 +0100

    Начал создавать функцию которая долэна запрашивать название и оценку фильма передавая это значение в объект.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 "use strict";
 
 let numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
+function ask() {
+
+	for (let i = 0; i < 2; i++) {
+		let movie = prompt('Один из последних просмотренных фильмов?');
+		let apprasial = prompt('На сколько его оцените?');
+		personalMovieDB.movies.movie = apprasial;
+	}
+}
+
 let personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
@@ -8,3 +17,6 @@ let personalMovieDB = {
 	genres: {},
 	privat: false
 }
+
+ask();
+console.log(personalMovieDB);
