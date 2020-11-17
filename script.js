accabed0894249d53a9094ab4306a8d7a5aa3523
antizyk <sashamatvienko0@gmail.com>
2020-11-17 10:59:02 +0100

    Добаввил коментарии к коду

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
-function ask() {
-
-	for (let i = 0; i < +numberOfFilms; i++) {
-		let movie = prompt('Один из последних просмотренных фильмов?');
-		let apprasial = prompt('На сколько его оцените?');
-		personalMovieDB.movies[`${movie}`] = apprasial;
+let numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");//Запрос о количестве просмотренных фильмов за последнее время
+function ask() {//Функция запрашивающая информацию о фильмах
+	for (let i = 0; i < +numberOfFilms; i++) {//Цикл зависящий от кол-ва просмотренных фильмов за последнее время
+		let movie = prompt('Один из последних просмотренных фильмов?');//Запрос о названии фильма
+		let apprasial = prompt('На сколько его оцените?');//Запрос об оценку этого фильма
+		personalMovieDB.movies[`${movie}`] = apprasial;//Создание объекта в объекте для размещения информации
 	}
 }
 
-let personalMovieDB = {
+let personalMovieDB = {//Объект со всей информацией по фильмам
 	count: numberOfFilms,
 	movies: {},
 	actors: {},
@@ -18,5 +17,5 @@ let personalMovieDB = {
 	privat: false
 }
 
-ask();
-console.log(personalMovieDB);
+ask();//Вызов функции
+console.log(personalMovieDB);//Вывод результата работы кода в консоль для проверке
