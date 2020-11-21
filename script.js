3d397ecf7ff1482c075c361ff7b49c9a1ce0553a
antizyk <sashamatvienko0@gmail.com>
2020-11-21 17:59:09 +0100

    Создал функцию, которая выводит объект базы жанных с фильмами в зависимости от установленной приватности

--- script.js
+++ script.js
@@ -3,28 +3,15 @@
 
 let numberOfFilms;
 
-function start() {
+
+//TODOO===========Создание функций==============================================
+function start() {//Запрос о количестве просмотренных фильмов за последнее время
 	while (numberOfFilms == null || numberOfFilms === '' || numberOfFilms > 50 || isNaN(numberOfFilms)) {
-		numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");//Запрос о количестве просмотренных фильмов за последнее время
+		numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
 	}
 	return numberOfFilms;
 }
-
-start();
-
-function ask() {//Функция запрашивающая информацию о фильмах
-	for (let i = 0; i < numberOfFilms; i++) {//Цикл зависящий от кол-ва просмотренных фильмов за последнее время
-		let movie = prompt('Один из последних просмотренных фильмов?');//Запрос о названии фильма
-		let apprasial = prompt('На сколько его оцените?');//Запрос об оценку этого фильма
-		if (movie != null && apprasial != null && movie != '' && apprasial != '' && movie.length < 50) {
-			personalMovieDB.movies[`${movie}`] = apprasial;//Создание объекта в объекте для размещения информации
-			console.log('Done');
-		} else {
-			console.log('Error');
-			i--;
-		}
-
-	}
+function detctPersonalLevel() {//Вывод уровня пользователя
 	if (personalMovieDB.count < 10) {
 		alert('Просмотренно довольно мало фильмов');
 	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
@@ -36,7 +23,29 @@ function ask() {//Функция запрашивающая информацию
 		alert('Произошла ошибка');
 	}
 }
+function ask() {//Функция запрашивающая информацию о фильмах
+	for (let i = 0; i < numberOfFilms; i++) {//Цикл зависящий от кол-ва просмотренных фильмов за последнее время
+		let movie = prompt('Один из последних просмотренных фильмов?');//Запрос о названии фильма
+		let apprasial = prompt('На сколько его оцените?');//Запрос об оценку этого фильма
+		if (movie != null && apprasial != null && movie != '' && apprasial != '' && movie.length < 50) {
+			personalMovieDB.movies[`${movie}`] = apprasial;//Создание объекта в объекте для размещения информации
+			console.log('Done');
+		} else {
+			console.log('Error');
+			i--;
+		}
+	}
+}
 
+function showMyDB() {
+	if (personalMovieDB.privat === false) {
+		console.log(personalMovieDB);
+	}
+}
+//TODOO=================================================================================
+
+
+start();
 let personalMovieDB = {//Объект со всей информацией по фильмам
 	count: numberOfFilms,
 	movies: {},
@@ -45,5 +54,9 @@ let personalMovieDB = {//Объект со всей информацией по 
 	privat: false
 }
 
-ask();//Вызов функции
+ask();
+detctPersonalLevel();
+showMyDB();
+
+
 console.log(personalMovieDB);//Вывод результата работы кода в консоль для проверке
