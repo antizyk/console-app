3ba2ee1ff78d21cb0afdeb62e0f8759096076f4a
antizyk <sashamatvienko0@gmail.com>
2020-11-21 16:45:50 +0100

    Усовершествовали цикл запроса названия и оценки фильмов, теперь ввод является обязательным.

--- script.js
+++ script.js
@@ -4,16 +4,31 @@
 
 const numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");//Запрос о количестве просмотренных фильмов за последнее время
 
-while (numberOfFilms === null || numberOfFilms.length > 50 || typeof numberOfFilms !== 'number') {
-	const numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
-}
+
 
 
 function ask() {//Функция запрашивающая информацию о фильмах
 	for (let i = 0; i < +numberOfFilms; i++) {//Цикл зависящий от кол-ва просмотренных фильмов за последнее время
 		let movie = prompt('Один из последних просмотренных фильмов?');//Запрос о названии фильма
 		let apprasial = prompt('На сколько его оцените?');//Запрос об оценку этого фильма
-		personalMovieDB.movies[`${movie}`] = apprasial;//Создание объекта в объекте для размещения информации
+		if (movie != null && apprasial != null && movie != '' && apprasial != '' && movie.length < 50) {
+			personalMovieDB.movies[`${movie}`] = apprasial;//Создание объекта в объекте для размещения информации
+			console.log('Done');
+		} else {
+			console.log('Error');
+			i--;
+		}
+
+	}
+	if (personalMovieDB.count < 10) {
+		alert('Просмотренно довольно мало фильмов');
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		alert('Вы класический зритель');
+	} else if (personalMovieDB.count >= 30) {
+		alert('Вы киноман');
+	}
+	else {
+		alert('Произошла ошибка');
 	}
 }
 
