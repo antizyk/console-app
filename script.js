f9b85bdcdb459f785eb510a067f249aa5e4d6575
antizyk <sashamatvienko0@gmail.com>
2020-11-22 19:10:56 +0100

    Сделал рефакторинг кода, теперь все функции стали методами объекта personalMovieDB

--- script.js
+++ script.js
@@ -1,69 +1,59 @@
 "use strict";
 
 
-let numberOfFilms;
-
-
-//TODOO===========Создание функций==============================================
-function start() {//Запрос о количестве просмотренных фильмов за последнее время
-	while (numberOfFilms == null || numberOfFilms === '' || numberOfFilms > 50 || isNaN(numberOfFilms)) {
-		numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
-	}
-	return numberOfFilms;
-}
-function detctPersonalLevel() {//Вывод уровня пользователя
-	if (personalMovieDB.count < 10) {
-		alert('Просмотренно довольно мало фильмов');
-	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-		alert('Вы класический зритель');
-	} else if (personalMovieDB.count >= 30) {
-		alert('Вы киноман');
-	}
-	else {
-		alert('Произошла ошибка');
-	}
-}
-function ask() {//Функция запрашивающая информацию о фильмах
-	for (let i = 0; i < numberOfFilms; i++) {//Цикл зависящий от кол-ва просмотренных фильмов за последнее время
-		let movie = prompt('Один из последних просмотренных фильмов?');//Запрос о названии фильма
-		let apprasial = prompt('На сколько его оцените?');//Запрос об оценку этого фильма
-		if (movie != null && apprasial != null && movie != '' && apprasial != '' && movie.length < 50) {
-			personalMovieDB.movies[`${movie}`] = apprasial;//Создание объекта в объекте для размещения информации
-			console.log('Done');
-		} else {
-			console.log('Error');
-			i--;
-		}
-	}
-}
-
-function showMyDB() {
-	if (personalMovieDB.privat === false) {
-		console.log(personalMovieDB);
-	}
-}
-
-function writeYourGeners() {
-	let answer;
-	for (let i = 0; i < 3; i++) {
-		answer = prompt(`Ваш любимый жанр под номером ${i + 1}?`);
-		personalMovieDB.genres.push(answer);
-	}
-}
-//TODOO=================================================================================
-
-
-start();
 let personalMovieDB = {//Объект со всей информацией по фильмам
-	count: numberOfFilms,
+	count: null,
 	movies: {},
 	actors: {},
 	genres: [],
-	privat: false
-}
+	privat: false,
+	start: function () {//Запрос о количестве просмотренных фильмов за последнее время
+		while (this.count == null || this.count === '' || this.count > 50 || isNaN(this.count)) {
+			this.count = +prompt("Сколько фильмов вы уже смотрели?");
+		}
+	},
+	detctPersonalLevel: function () {//Вывод уровня пользователя
+		if (this.count < 10) {
+			alert('Просмотренно довольно мало фильмов');
+		} else if (this.count >= 10 && this.count < 30) {
+			alert('Вы класический зритель');
+		} else if (this.count >= 30) {
+			alert('Вы киноман');
+		}
+		else {
+			alert('Произошла ошибка');
+		}
+	},
+	ask: function () {//Функция запрашивающая информацию о фильмах
+		for (let i = 0; i < this.count; i++) {//Цикл зависящий от кол-ва просмотренных фильмов за последнее время
+			let movie = prompt('Один из последних просмотренных фильмов?');//Запрос о названии фильма
+			let apprasial = prompt('На сколько его оцените?');//Запрос об оценку этого фильма
+			if (movie != null && apprasial != null && movie != '' && apprasial != '' && movie.length < 50) {
+				personalMovieDB.movies[`${movie}`] = apprasial;//Создание объекта в объекте для размещения информации
+				console.log('Done');
+			} else {
+				console.log('Error');
+				i--;
+			}
+		}
+	},
+	showMyDB: function () {
+		if (personalMovieDB.privat === false) {
+			console.log(personalMovieDB);
+		}
+	},
 
-ask();
-showMyDB();
-writeYourGeners();
-detctPersonalLevel();
+	writeYourGeners: function () {
+		let answer;
+		for (let i = 0; i < 3; i++) {
+			answer = prompt(`Ваш любимый жанр под номером ${i + 1}?`);
+			personalMovieDB.genres.push(answer);
+		}
+	}
+};
+personalMovieDB.start();
+personalMovieDB.ask();
+personalMovieDB.showMyDB();
+personalMovieDB.writeYourGeners();
+personalMovieDB.detctPersonalLevel();
 
